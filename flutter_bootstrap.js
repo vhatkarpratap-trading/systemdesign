d129b9370339e25de867c6a5176a62c659784636
vhatkarpratap-trading <vhatkarpratap@gmail.com>
2026-02-05 07:17:45 +0000

    Deploying to gh-pages from @ vhatkarpratap-trading/systemdesign@d648383e46fb66bb675f2b3ece5e0b688a9954aa 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"587c18f873b8ab57330422bce09047420d9c7f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "886305049"
+    serviceWorkerVersion: "1065310062"
   }
 });
